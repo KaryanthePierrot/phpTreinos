5371d8a48a5f02f676d0220dd9edfa96f389c30a
KaryanthePierrot <mesquita.westesoficial@gmail.com>
2025-11-07 13:28:01 -0300

    composer

---
+++ wes/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => 'wes/tasklist',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '440a65bc4f1c8341afa4de4de7603dae6aa8686c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'phpmailer/phpmailer' => array(
+            'pretty_version' => 'v7.0.0',
+            'version' => '7.0.0.0',
+            'reference' => 'c7111310c6116ba508a6a170a89eaaed2129bd42',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../phpmailer/phpmailer',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'wes/tasklist' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '440a65bc4f1c8341afa4de4de7603dae6aa8686c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
